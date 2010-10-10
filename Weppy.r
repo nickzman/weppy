974e18ee2b912e181c2bee32a1912bbd998e80cd
Nick Zitzmann <nickzman@Gorgimera.local>
2010-10-10 17:33:36 -0600

    Fixed an incompatibility with Opera. Opera, as it turns out, pretends we don't exist unless we define our MIME types in a resource file

---
+++ Weppy.r
@@ -0,0 +1,27 @@
+#include <Carbon/Carbon.r>
+
+// Opera refuses to acknowledge the existence of our plugin unless we register it the old-fasioned way...
+resource 'STR#' (126)
+{
+	{
+		"WebP browser plugin for Mac OS X",
+		"Weppy",
+	}
+};
+
+
+resource 'STR#' (127)
+{
+	{
+		"WebP Image"
+	}
+};
+
+
+resource 'STR#' (128)
+{
+	{
+		"image/webp",
+		"webp"
+	}
+};
